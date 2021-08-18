e3a48c9eea2a2959ec58e94c608af16af1e7e325
Suyash Gupta <suyashgupta@dhcp-10-7-36-226.dhcp.nd.edu>
2021-08-18 15:12:41 -0400

    Commiting Trial

--- GitHub Trial.R
+++ GitHub Trial.R
@@ -1,1 +1,13 @@
-y <- 1
+install.packages(c("car", "ggplot2"))
+library(ggplot2)
+my_plot <- ggplot(data = mtcars, 
+       mapping = aes(x = disp, y = mpg)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+install.packages(c("plotly"))
+
+plotly::ggplotly(my_plot)
+
+st_joe_land <- read.delim(file = "stJoeLand.csv", sep = ",")
+
+st_joe_land <- read.csv(file = "stJoeLand.csv")
